80e0b09e6658ddf707e556a23eeca3632346d1e7
Aline-G-Monroy <gmonroys2200@alumno.ipn.mx>
2024-11-03 15:11:17 -0600

    cambios menores

--- src/components/Producto/Producto.jsx
+++ src/components/Producto/Producto.jsx
@@ -9,12 +9,12 @@ export default function Producto (props){
     const funcion = () => {};
 
     return (
-        <div className="flex bg-a-300 h-60 w-[700px]">
+        <div className="flex h-60 w-[700px] rounded-[8px] overflow-hidden">
             <img className="img-fluid size-60" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS08JbeYZ8ccPOp4Su1QuQ6xJueP1D-0XFYgA&s" alt="Producto imagen" />
-            <div className="flex flex-col py-4 px-6 flex-1 gap-4 justify-between">
+            <div className="flex flex-col py-4 px-6 flex-1 gap-4 justify-between bg-g-50">
                 <div className="gap-2">
                     <div className="col-12 font-bold">{props.nombre}</div>
-                    <div className="col-12 text-xs text-justify">{props.detalles}</div>
+                    <div className="col-12 text-xs text-justify line-clamp-3 ">{props.detalles}</div>
                 </div>
                 <div className="flex align-middle justify-between">
                     <div className="flex justify-items-start gap-2">
