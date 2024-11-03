51a1a85395b7ee9b9f4a541f7498c48a321ed5bf
Aline-G-Monroy <gmonroys2200@alumno.ipn.mx>
2024-11-03 14:05:01 -0600

    cambios menores

--- src/components/Producto/Producto.jsx
+++ src/components/Producto/Producto.jsx
@@ -11,26 +11,26 @@ export default function Producto (props){
     return (
         <div className="flex bg-a-300 h-60 w-[735px]">
             <img className="img-fluid size-60" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS08JbeYZ8ccPOp4Su1QuQ6xJueP1D-0XFYgA&s" alt="Producto imagen" />
-            <div className="flex-col p-6 w-[495px] gap-4">
+            <div className="flex flex-col py-4 px-6 w-[495px] gap-4 justify-between">
                 <div className="col-12 font-bold">{props.nombre}</div>
                 <div className="col-12 text-xs text-justify">{props.detalles}</div>
                 <div className="flex align-middle justify-between">
                     <div className="flex justify-items-start gap-2">
-                        <div>%{props.descuento}</div>
-                        <div>${props.precio}</div>
+                        <div className="text-p-600 font-bold">-{props.descuento}%</div>
+                        <div className="font-bold">${props.precio}</div>
                     </div>
                     <Rating rating={4}></Rating>
                 </div>
 
-                <div className="flex gap-[10px]">
+                <div className="flex gap-[10px] justify-between">
                     <div className="w-[104px]">
                         <NumerInput response={funcion}></NumerInput>
                     </div>
-                    <PrimaryButton size="233px" onClick={() => {
+                    <PrimaryButton size={"[233px]"} onClick={() => {
                         alert("Comprado");}}>Comprar </PrimaryButton>
-                    <PrimaryButton size="10rem" onClick={() => {
+                    <PrimaryButton size={"10"} onClick={() => {
                         alert("Carrito");}}>icono </PrimaryButton>
-                    <PrimaryButton size="10rem" onClick={() => {
+                    <PrimaryButton size={"10"} onClick={() => {
                         alert("WishList");}}>icono </PrimaryButton>
                 </div>
             </div>
@@ -40,7 +40,7 @@ export default function Producto (props){
 
 Producto.defaultProps = {
     nombre: "Producto",
-    detalles: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis ex lorem. Nunc molestie, urna sed euismod dapibus, magna sapien fringilla nisi, ornare egestas justo mi non quam. Aenean vehicula purus vitae varius facilisis. Donec eget maximus quam. Etiam sed nibh quis neque maximus tincidunt. Mauris sed felis porta, fermentum orci nec, fermentum tortor.",
+    detalles: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis ex lorem. Nunc molestie, urna sed euismod dapibus, magna sapien fringilla nisi, ornare egestas justo mi non quam. Aenean vehicula purus vitae varius facilisis.",
     precio: 999.99,
     descuento: 20,
 }
