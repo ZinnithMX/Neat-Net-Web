f3e26d24bea30df818aeeca7840f65b0de9d4843
Aline-G-Monroy <gmonroys2200@alumno.ipn.mx>
2024-11-03 20:12:50 -0600

    Detallitos

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="bg-n-700 h-[60px] flex flex-row justify-center items-center justify-between p-[24px] text-a-50 text-base">
+        <footer className="bg-n-700 h-[60px] flex flex-row justify-center items-center justify-between p-[24px] text-g-50 text-base">
             <div className="text-center">Términos y Condiciones</div>
             <div className="text-center">Política de privacidad</div>
             <div className="text-center">Soporte Técnico</div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,18 +3,18 @@ import {useState} from "react";
 
 export default function Header() {
     return (
-        <header className="w-full h-16">
+        <header className="w-full h-16 items-center">
             <div className="flex flex-row w-full items-center justify-between pl-6 pr-6">
                 <div className="text-p-600 w-1/4"><h4>Neat Net</h4></div>
                 <div className="w-1/2">
                     <Input required={false} deshabilitado={false} validate={false} response={false}>Buscar
                         producto</Input>
                 </div>
-                <div className="flex justify-end w-1/4 gap-4">
+                <div className="flex justify-end w-1/4 gap-4 items-center">
                     <span className={"material-symbols-rounded icon text-n-200"}>shopping_cart</span>
                     <span className={"material-symbols-rounded icon text-n-200"}>list</span>
                     <span className={"material-symbols-rounded icon text-n-200"}>notifications</span>
-                    <span className={"material-symbols-rounded icon text-n-200"}>account_circle</span>
+                    <span className={"material-symbols-rounded icon text-n-200 rounded-full bg-g-300 p-1"}>account_circle</span>
                 </div>
             </div>
         </header>
