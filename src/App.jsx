85cfa94dcc5ca77295dce122d8e54054a1b1a8a2
CarritoHG <hhernandezc2202@alumno.ipn.mx>
2024-11-05 22:58:21 -0600

    Ajuste de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,29 @@ import SignupComprador from "./pages/account/signup/SignupComprador"
 import Playground from "./pages/playground.jsx";
 import Producto from "./components/Producto/Producto.jsx";
 import Productos from "./pages/productos/Productos.jsx";
+import SignupVendedor from "./pages/account/signup/SingupVendedor.jsx";
 
 function App() {
 
     return (
         <>
-          <Router>
-            <Routes>
-              <Route path="/" element={<Productos/>}/>
-                <Route path={"/play"} element={<Playground/>}/>
-            </Routes>
-          </Router>
+            <Router>
+                <Routes>
+                    <Route index element={<Promo/>}/>
+                    <Route path={"*"} element={<NotFound/>}/>
+                    <Route path={"/play"} element={<Playground/>}/>
+                    <Route path={"/TOS"} element={<TOS/>}/>
+                    <Route path={"/Privacy"} element={<Privacy/>}/>
+                    <Route path={"/signup/comprador"} element={<SignupComprador/>}/>
+                    <Route path={"/signup/vendedor"} element={<SignupVendedor/>}/>
+                    <Route path={"/login/comprador"} element={<LoginComprador/>}/>
+                    <Route path={"/login/vendedor"} element={<LoginVendedor/>}/>
+                    <Route path={"/productos"} element={<Productos/>}/>
+                    <Route path={"/productos/:id"} element={<VerProducto/>}/>
+                    <Route path={"/buscar/producto/"} element={<BuscarProducto/>}/>
+                    <Route path={"/cuenta"} element={<Perfil/>}/>
+                </Routes>
+            </Router>
         </>
 
     )
