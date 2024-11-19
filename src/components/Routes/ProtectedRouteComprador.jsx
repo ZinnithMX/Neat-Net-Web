ba6fac2c9bf04aeebbc1ad9397e9a47ba6f87d21
Carlos Marin <candresm2200@alumno.ipn.mx>
2024-11-18 19:08:18 -0600

    Comprobacion continua de sesion abierta

--- src/components/Routes/ProtectedRouteComprador.jsx
+++ src/components/Routes/ProtectedRouteComprador.jsx
@@ -1,7 +1,7 @@
 import {Cookies} from "react-cookie";
 import {Navigate, Outlet} from "react-router-dom";
 import axios from "axios";
-import {useContext} from "react";
+import {useContext, useEffect} from "react";
 import {DomainContext} from "../../App.jsx";
 
 export default function ProtectedRouteComprador({children, redirectTo}){
@@ -10,30 +10,34 @@ export default function ProtectedRouteComprador({children, redirectTo}){
     const domain = useContext(DomainContext);
 
 
-    if(!userCookie.get("sesionId")){
-        return <Navigate to={redirectTo}/>
-    }
-    else{
-        const url = domain + ":8080/login/sessionId?" + new URLSearchParams({
-            sessionId: userCookie.get("sesionId")
-        });
-        const headers = new Headers();
-        const encodedCredentials = btoa(`${"Ingreso"}:${"visitante"}`);
-        headers.append("Authorization", `Basic ${encodedCredentials}`);
-        headers.append("Content-Type", `application/json`);
-
-        axios.get(url, {headers: headers}).then(res => {
-            if(res.status === 200){
-                userCookie.set("idUsuario", res.data.idUsuario, {path: "/"});
-            }
-            else{
-                userCookie.remove("sesionId", {path: "/"});
-                return <Navigate to={redirectTo}/>
-            }
-        }).catch(err => {
+    useEffect(() => {
+        if(!userCookie.get("sesionId")){
             return <Navigate to={redirectTo}/>
-        })
-    }
+        }
+        else{
+            const url = domain + ":8080/login/sessionId?" + new URLSearchParams({
+                sessionId: userCookie.get("sesionId")
+            });
+            const headers = new Headers();
+            const encodedCredentials = btoa(`${"Ingreso"}:${"visitante"}`);
+            headers.append("Authorization", `Basic ${encodedCredentials}`);
+            headers.append("Content-Type", `application/json`);
+
+            axios.get(url, {headers: headers}).then(res => {
+                if(res.status === 200){
+                    userCookie.set("idUsuario", res.data.idUsuario, {path: "/"});
+                }
+                else{
+                    userCookie.remove("sesionId", {path: "/"});
+                    return <Navigate to={redirectTo}/>
+                }
+            }).catch(err => {
+                return <Navigate to={redirectTo}/>
+            })
+        }    
+    }, [userCookie]);
+
+
 
     return <Outlet/>
 
