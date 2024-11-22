bd9a940ad28f11bb2010f25bb49a100ac09e8bdf
Carlos Marin <candresm2200@alumno.ipn.mx>
2024-11-21 20:35:06 -0600

    [FIX] Cookie reload

--- src/components/Routes/ProtectedRouteComprador.jsx
+++ src/components/Routes/ProtectedRouteComprador.jsx
@@ -10,32 +10,30 @@ export default function ProtectedRouteComprador({children, redirectTo}){
     const domain = useContext(DomainContext);
 
 
-    useEffect(() => {
-        if(!userCookie.get("sesionId")){
-            return <Navigate to={redirectTo}/>
-        }
-        else{
-            const url = domain + ":8080/login/sessionId?" + new URLSearchParams({
-                sessionId: userCookie.get("sesionId")
-            });
-            const headers = new Headers();
-            const encodedCredentials = btoa(`${"Ingreso"}:${"visitante"}`);
-            headers.append("Authorization", `Basic ${encodedCredentials}`);
-            headers.append("Content-Type", `application/json`);
+    if(!userCookie.get("sesionId")){
+        return <Navigate to={redirectTo}/>
+    }
+    else{
+        const url = domain + ":8080/login/sessionId?" + new URLSearchParams({
+            sessionId: userCookie.get("sesionId")
+        });
+        const headers = new Headers();
+        const encodedCredentials = btoa(`${"Ingreso"}:${"visitante"}`);
+        headers.append("Authorization", `Basic ${encodedCredentials}`);
+        headers.append("Content-Type", `application/json`);
 
-            axios.get(url, {headers: headers}).then(res => {
-                if(res.status === 200){
-                    userCookie.set("idUsuario", res.data.idUsuario, {path: "/"});
-                }
-                else{
-                    userCookie.remove("sesionId", {path: "/"});
-                    return <Navigate to={redirectTo}/>
-                }
-            }).catch(err => {
+        axios.get(url, {headers: headers}).then(res => {
+            if(res.status === 200){
+                userCookie.set("idUsuario", res.data.idUsuario, {path: "/"});
+            }
+            else{
+                userCookie.remove("sesionId", {path: "/"});
                 return <Navigate to={redirectTo}/>
-            })
-        }
-    }, [userCookie]);
+            }
+        }).catch(err => {
+            return <Navigate to={redirectTo}/>
+        })
+    }
 
 
 
