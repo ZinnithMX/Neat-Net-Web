dc02f54350faa277090dbccc1933147ebaf44479
Carlos Marin <candresm2200@alumno.ipn.mx>
2024-12-03 23:49:38 -0600

    rutas protegidas vendedor

--- src/components/Routes/ProtectedRouteVendedor.jsx
+++ src/components/Routes/ProtectedRouteVendedor.jsx
@@ -24,7 +24,23 @@ export default function ProtectedRouteComprador({children, redirectTo}){
 
         axios.get(url, {headers: headers}).then(res => {
             if(res.status === 200){
-                userCookie.set("idUsuario", res.data.idUsuario, {path: "/"});
+                const usuario = res.data.usuario;
+                const params = JSON.stringify(usuario);
+                const url = domain + ":8080/login/verificarVendedor?" + new URLSearchParams({
+                    params
+                })
+                const headers = new Headers();
+                const encodedCredentials = btoa(`${"Ingreso"}:${"visitante"}`);
+                headers.append("Authorization", `Basic ${encodedCredentials}`);
+                headers.append("Content-Type", `application/json`);
+
+                axios.get(url, {headers: headers}).then(res =>{
+                    if(res.status === 200){
+                        userCookie.set("idUsuario", res.data.idUsuario, {path: "/"});
+                    }else{
+                        return <Navigate to={"/productos/"}/>
+                    }
+                })
             }
             else{
                 userCookie.remove("sesionId", {path: "/"});
